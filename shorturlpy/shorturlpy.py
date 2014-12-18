e44cb6cc75e597d9bfa945625e2b7e8e006ef616
Abdul Hamid <ah@appscluster.com>
2014-12-18 14:50:05 +0000

    User-Agent added

--- shorturlpy/shorturlpy.py
+++ shorturlpy/shorturlpy.py
@@ -7,11 +7,11 @@
 import sys, time, inspect, os.path, getopt
 import urllib2, urllib
 
-HEADERS = {'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36'}
-HEADERS_MOBILE = {'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'}
+HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36'}
+HEADERS_MOBILE = {'User-Agent':'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'}
 
 MAX_ATTEMPTS = 3
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 
 class ShortUrlPy():
     """docstring foShortUrlPyme"""
